24e789b0c57905f22742d4ce56541a8eda6189c8
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-03-05 16:17:30 +0200

    add  app folders

---
+++ start.sh
@@ -0,0 +1,5 @@
+export QUOTE_API_BASE_URL = "http://quotes.stormconsultancy.co.uk/random.json"
+export SECRET_KEY=0787889107
+export MAIL_USERNAME='angelakarenzi5@gmail.com'
+export MAIL_PASSWORD='GodAlmighty1'
+python3.6 manage.py server
